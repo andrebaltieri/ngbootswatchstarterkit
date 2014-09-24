d48eaaa90350e142002eb64f290d6c5206688ef3
Andre Baltieri <andrebaltieri@hotmail.com>
2014-09-24 14:50:38 -0300

    Version 1.0.0

---
+++ NgStarterKit/app/config.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    'use strict';
+    var id = 'app';
+
+    var app = angular.module('app', ['ngRoute', 'ngAnimate']);
+
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'HomeCtrl',
+                templateUrl: 'app/home/views/index.html'
+            })
+            .when('/icons', {
+                controller: 'HomeCtrl',
+                templateUrl: 'app/home/views/icons.html'
+            })
+            .otherwise({
+                controller: 'HomeCtrl as vm',
+                templateUrl: 'app/views/404.html'
+            });
+    });
+
+    app.controller('AppCtrl', function AppCtrl($scope, $timeout) {
+
+        // This theme function is used only for example
+        // please choose a theme and fix the "bootstrap-{{ theme }}.min.css"
+        // on the index.html
+        $scope.theme = 'default';
+
+        $scope.changeTheme = function(theme) {
+            $scope.theme = theme;
+        };
+    });
+})();
---
+++ NgStarterKit/app/home/controllers/HomeCtrl.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('HomeCtrl', HomeCtrl);
+
+    HomeCtrl.$inject = ['$location']; 
+
+    function HomeCtrl($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'HomeCtrl';
+
+        activate();
+
+        function activate() { }
+    }
+})();
